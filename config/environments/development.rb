4b46c2fc73aee118cc9987efdb5e9c96517bafdf
Michael A Schwartz <mikalan2001@gmail.com>
2014-02-22 02:23:22 -0500

    Add SSL in production

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,4 +26,5 @@ SampleProject::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+  config.force_ssl = true
 end
